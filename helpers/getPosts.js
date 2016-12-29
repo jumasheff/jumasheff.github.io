47ef6e6d88f85e6a24c5339ba8a0d3b4af934f7d
Murat Jumashev <jumasheff@gmail.com>
2016-12-30 01:36:31 +0600

    Add helper for getting filenames from posts dir

---
+++ helpers/getPosts.js
@@ -0,0 +1,8 @@
+
+function getPosts() {
+    const req = require.context("../posts", true, /^\.\/.*\.md$/);
+    const dirtyFileNames = req.keys();
+    return dirtyFileNames.map(f => f.split('/').pop())
+}
+
+export default getPosts;
