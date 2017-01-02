60a4c234dcc45032d0550876890cb3ad163f548f
Murat Jumashev <jumasheff@gmail.com>
2017-01-02 17:04:51 +0600

    Add slugified category and slugified url

--- helpers/getPosts.js
+++ helpers/getPosts.js
@@ -9,8 +9,9 @@ function getPosts() {
         const filename = spl.slice(-1).pop();
         const category = spl.slice(1)[0];
         const title = filename.split('.md')[0];
-        const slug = urlSlug(title);
-        return { filename, category, title, slug }
+        const slugifiedCaterory = urlSlug(category);
+        const slugifiedUrl = urlSlug(title);
+        return { filename, category, title, slugifiedCaterory, slugifiedUrl }
     })
 }
 
