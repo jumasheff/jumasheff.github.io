8715cdb756a28f5b642c1e4544c9cabdf04fba99
Murat Jumashev <jumasheff@gmail.com>
2017-03-11 19:35:52 +0600

    Render all posts in Home component

--- components/Home.js
+++ components/Home.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router';
+import React from 'react'
+import Posts from './Posts'
 
 function Home() {
   const repoReadmeLink = 'https://github.com/rafrex/spa-github-pages#readme';
   return (
     <div>
-      <p>
-        Сайтта курулуш иштери жүргүзүлүүдө.
-      </p>
+      <Posts />
       <p>
         Бул долбоор GitHub Pages'те орнотулган жана <a href={repoReadmeLink}>бул </a>
         калыптын негизинде кураштырылган.
       </p>
-      <p>Төмөнкүлөр бул жерде жөн эле турат :)</p>
-      <div><Link to="/posts/zhalpy/test">Sample post</Link></div>
-      <div><Link to="/categories/kategoriyasyz">Sample category</Link></div>
-      <div><Link to="/example">Example page</Link></div>
-      <div><Link to="/example/two-deep?field1=foo&field2=bar#boom!">
-        Example two deep with query and hash
-      </Link></div>
     </div>
   );
 }
